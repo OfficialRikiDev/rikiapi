bf3d001948caf39957ed2509ccc7863220b05e75
RikiDev <ricpino585@gmail.com>
2022-08-24 16:34:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
     print_r($data);
 
     print_r($_GET);
+
+    $output = implode(', ', array_map(
+        function ($v, $k) { return sprintf("%s='%s'", $k, $v); },
+        $_GET,
+        array_keys($_GET)
+    ));
     
-    $url = "https://" . $data[1]. ".roblox.com/". $data[0] . "/" .  implode("/",array_slice($data,1));
+    $url = "https://" . $data[1]. ".roblox.com/". $data[0] . "/" .  implode("/",array_slice($data,1)) . "" . $output;
 
     echo $url;
 ?>
