a1855f68542f3c1c6325cac7e6c52b4f8460b02a
RikiDev <ricpino585@gmail.com>
2022-08-24 16:50:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,25 @@
     ));
     
     $url = "https://" . $data[1]. ".roblox.com/". $data[0] . "/" .  implode("/",array_slice($data,1)) . "?" . $output;
-    $post = array('key' => "adminpinohehe");
+    $request_headers = array(
+                    "Key: adminpinohee",
+                );
+
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-    $response = curl_exec($ch);
-    $result = json_decode($response);
-    curl_close($ch); // Close the connection
-    echo $result;
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
+
+    $season_data = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        print "Error: " . curl_error($ch);
+        exit();
+    }
+
+    // Show me the result
+    curl_close($ch);
+    $json= json_decode($season_data, true);
+    echo $json;
    
 ?>
