db3bb9606f4e8907f1c8db8b67a2dc1d4a67542a
RikiDev <ricpino585@gmail.com>
2022-08-24 15:42:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     $key = getenv('secret_key');
-    $args = $_SERVER["REQUEST_URI"];
-    $arg_arr = explode("/",$args);
+    $args = $_SERVER["PATH_INFO"];
+    $arg_arr = explode("/",$args, "/");
     print_r($arg_arr);
 
     print_r($_GET);
