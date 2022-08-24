ee339f26c08a7b6fa6fae599e340c25991f6604e
RikiDev <ricpino585@gmail.com>
2022-08-24 16:45:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
     ));
     
     $url = "https://" . $data[1]. ".roblox.com/". $data[0] . "/" .  implode("/",array_slice($data,1)) . "?" . $output;
-
-    
+    $post = array('key' => "adminpinohehe");
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+    $response = curl_exec($ch);
+    $result = json_decode($response);
+    curl_close($ch); // Close the connection
+    if($result)
+    {
+      echo $result;
+    }
+    else 
+    {
+      http_response_code(407)
+    }
 ?>
